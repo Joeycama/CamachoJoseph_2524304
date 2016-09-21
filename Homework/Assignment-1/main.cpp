7b88c00c7e927a5ef3ffeca47d92e67cac154f4e
Joeycama <Joey92805@gmail.com>
2016-09-20 23:09:00 -0700

    Add files via upload

---
+++ Homework/Assignment-1/main.cpp
@@ -0,0 +1,35 @@
+/* 
+   File:   main
+   Author: Joseph Camacho
+   Created on September 20, 2016, 10:37 AM
+   Purpose:
+ */
+
+//System Libraries
+#include <iostream>   
+using namespace std;  
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declaration of Variables
+    short a,b,c;
+    
+    //Set values
+    a=50;
+    b=100;
+    
+    //Process values
+    c=a+b;
+    
+    //Display Output
+    cout<<a<<" + "<<b<<" = "<<c<<endl;
+    cout<<"Total: "<<c<<endl;
+    //Exit Program
+    return 0;
+}
